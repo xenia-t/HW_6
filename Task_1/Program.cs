efe1e079110f9f7fd675da4018d9d39577dfe910
Xenia <listk98@yandex.ru>
2022-03-31 03:06:55 +0300

    Add Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,56 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввел пользователь.
+
+int M = Promt("\nВведите длину массива: ");
+Console.WriteLine();
+
+if (M > 0)
+{
+    int[] array = InputArray(M);
+    PrintArray(array);
+    Console.WriteLine($"Количество чисел больше 0 составляет {cout(array)}\n");
+}
+
+else Console.WriteLine("Введено некорректное значение длины массива\n");
+
+
+int Promt(String message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int[] InputArray (int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"Введите {i} элемент массива ");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("\nМассив: [ ");
+    foreach (int digit in array)
+    {
+        Console.Write(digit + " ");
+    }
+    Console.WriteLine("]\n");
+}
+
+int cout(int[] array)
+{
+    int cout = 0;
+    foreach (int digit in array)
+    {
+        if (digit > 0) cout++;
+    }
+    return cout;
+}
+
+
+
